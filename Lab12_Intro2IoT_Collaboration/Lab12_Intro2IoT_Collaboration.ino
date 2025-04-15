0e121ab5d5c2753d37cd91a655931eb759734d56
Felix692lul <felix-shumo.sun@dawsoncollege.qc.ca>
2025-04-15 10:38:21 -0400

    Update
    ...

--- Lab12_Intro2IoT_Collaboration/Lab12_Intro2IoT_Collaboration.ino
+++ Lab12_Intro2IoT_Collaboration/Lab12_Intro2IoT_Collaboration.ino
@@ -34,12 +34,12 @@ void loop() {
 // ===============================
 // Light Detection – Student 1
 bool isRoomDark() {
-  int lightLevel = analogRead(lightSensorPin);
-  Serial.print("Light Level: ");
-  Serial.println(lightLevel);
+  int lightlevel= analogRead(A0);
+  Serial.print("Lightlevel: ");
+  Serial.println(lightlevel);
 
   // TODO: Fix this logic so it returns true when it's dark
-  return false; // <-- incorrect for now
+  return true; // <-- incorrect for now
 }
 
 // ===============================
