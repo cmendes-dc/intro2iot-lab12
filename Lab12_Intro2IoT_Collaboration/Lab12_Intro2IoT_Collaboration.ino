556bc75f3568bf5ca601631c9182149c297d0302
Eva-1337 <william.bouchard@dawsoncollege.qc.ca>
2025-04-22 09:38:30 -0400

    Fix Button logic
    changed the logic so the button is off when not pressed

--- Lab12_Intro2IoT_Collaboration/Lab12_Intro2IoT_Collaboration.ino
+++ Lab12_Intro2IoT_Collaboration/Lab12_Intro2IoT_Collaboration.ino
@@ -47,11 +47,11 @@ bool isRoomDark() {
 bool isButtonActivated() {
   bool state = digitalRead(buttonPin);
   
-  Serial.print("Button: ");
+  Serial.println("Button: ");
 
   // TODO: Fix this logic and print the correct status
   // Button is normally HIGH
-  if (state == HIGH) {
+  if (state == LOW) {
     // Button not pressed
     return true;
   } else {
